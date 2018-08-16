737d7a12800b30bef3518fe1ed00e25f15bdc6d3
rpass <robertmpassmore@gmail.com>
2018-08-16 20:29:33 +0200

    add basic UrlShortener

---
+++ lib/url_shortener.rb
@@ -0,0 +1,9 @@
+require 'digest'
+
+class UrlShortener
+	def self.shorten(url)
+		# SHA256 hashing is okay for now, at larger volumes, speed of hashing
+		# algorithm might justify changing to a faster algorithm
+		Digest::SHA256.hexdigest(url)[0..6]
+	end
+end
---
+++ test/url_shortener_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "./lib/url_shortener.rb"
+
+class TestUrlShortener < Minitest::Test
+
+	def test_shorten_takes_a_url_and_returns_first_six_characters_of_sha256_hexdigest
+		url = 'http://www.google.com'
+
+		# hex digest of SHA256 hash of http://www.google.com
+		expected_short_url = '253d142'
+
+		actual_ouput = UrlShortener.shorten(url)
+
+		assert_equal expected_short_url, actual_ouput
+	end
+end
