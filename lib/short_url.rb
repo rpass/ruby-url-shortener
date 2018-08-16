e69d96e9aea417e0af8640c00db03af3bde6f273
rpass <robertmpassmore@gmail.com>
2018-08-16 20:36:15 +0200

    add url reading to ShortUrl

--- lib/short_url.rb
+++ lib/short_url.rb
@@ -1,4 +1,6 @@
 class ShortUrl
+	attr_reader :url
+	
 	def initialize(url)
 		@url = url
 		@short_url = 'asdf123'
--- test/short_url_test.rb
+++ test/short_url_test.rb
@@ -10,4 +10,12 @@ class TestExample < Minitest::Test
 
 		refute_nil short_url
 	end
+
+	def test_url_returns_the_original_construction_argument
+		url = 'http://www.google.com'
+
+		short_url = ShortUrl.new(url)
+
+		assert_equal url, short_url.url
+	end
 end
