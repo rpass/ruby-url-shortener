6cb232632e3760c3152fb72998d677a2f463e6c4
rpass <robertmpassmore@gmail.com>
2018-08-16 19:54:20 +0200

    add dummy POST endpoint to test interface

--- lib/app.rb
+++ lib/app.rb
@@ -11,5 +11,21 @@ require 'json'
 # { short_url: 'asdf', url: 'http://www.google.com' }
 get '/:short_url' do
 	short_url = params[:short_url]
-	short_url	
+	short_url
+end
+
+##
+# This endpoint accepts POST request body of
+# a url and returns a JSON response including 
+# the original url and the shortened url
+#
+# accepts POST requests with JSON body
+# returns JSON response with `url` and `short_url`
+post '/' do
+	url = params[:url]
+	
+	{
+		short_url: 'asdf123',
+		url: url
+	}.to_json
 end 
--- test/app_test.rb
+++ test/app_test.rb
@@ -20,4 +20,18 @@ class AppTest < MiniTest::Test
 		assert last_response.ok?
 		assert_equal expected_response, last_response.body
 	end
+
+	def test_get_long_url_with_short_url_path_param
+		example_long_url = 'http://www.google.com'
+
+		post '/', {url: example_long_url}
+
+		expected_response = {
+			short_url: 'asdf123',
+			url: example_long_url
+		}.to_json
+
+		assert last_response.ok?
+		assert_equal expected_response, last_response.body
+	end
 end
