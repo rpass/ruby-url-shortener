da1b81308097da4327caf37cc78215ef1e56a44b
rpass <robertmpassmore@gmail.com>
2018-08-16 19:51:16 +0200

    add dummy GET endpoint to test setup

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'json'
 
-get '/' do
-	'Hello World!'
-end
+##
+# This endpoint returns the long (original) url when
+# supplied with a short url as a path parameter
+#
+# usage:
+# accepts requests like GET /asdf
+# returns a 301 Permanently Remove redirect response
+# { short_url: 'asdf', url: 'http://www.google.com' }
+get '/:short_url' do
+	short_url = params[:short_url]
+	short_url	
+end 
--- test/app_test.rb
+++ test/app_test.rb
@@ -3,6 +3,8 @@ require 'rack/test'
 require 'sinatra'
 require './lib/app.rb'
 
+require 'json'
+
 class AppTest < MiniTest::Test
 	include Rack::Test::Methods
 
@@ -10,10 +12,12 @@ class AppTest < MiniTest::Test
 		Sinatra::Application
 	end
 
-	def test_hello_world
-		get '/'
+	def test_get_long_url_with_short_url_path_param
+		get '/asdf123'
+
+		expected_response = 'asdf123'
 
 		assert last_response.ok?
-		assert_equal 'Hello World!', last_response.body
+		assert_equal expected_response, last_response.body
 	end
 end
